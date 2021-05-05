f2624ebd8b29586218eddbbf2d4c283b854c2e65
moamen-gamal <moameneisa@yahoo.com>
2021-05-05 18:13:04 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,36 @@
 #include <cmath>
 #include<iostream>
 #include<array>
+#include<complex.h>
+#include<ccomplex>
+#include<cmath>
+#define I _Complex_I // can be used to redefine I
 using namespace std;
+
+// Trick to allow type promotion below
+template <typename T>
+struct identity_t { typedef T type; };
+
+/// Make working with std::complex<> nubmers suck less... allow promotion.
+#define COMPLEX_OPS(OP)                                                 \
+  template <typename _Tp>                                               \
+  std::complex<_Tp>                                                     \
+  operator OP(std::complex<_Tp> lhs, const typename identity_t<_Tp>::type & rhs) \
+  {                                                                     \
+    return lhs OP rhs;                                                  \
+  }                                                                     \
+  template <typename _Tp>                                               \
+  std::complex<_Tp>                                                     \
+  operator OP(const typename identity_t<_Tp>::type & lhs, const std::complex<_Tp> & rhs) \
+  {                                                                     \
+    return lhs OP rhs;                                                  \
+  }
+COMPLEX_OPS(+)
+COMPLEX_OPS(-)
+COMPLEX_OPS(*)
+COMPLEX_OPS(/ )
+#undef COMPLEX_OPS
+
 template<size_t T>
 
 void fourir(array<double, T> input_data, array<double, T>& real, array<double, T>& imag)
@@ -30,16 +59,16 @@ void fourir(array<double, T> input_data, array<double, T>& real, array<double, T
     
     return;
 }
+double t_real;
+double t_imag;
 
 void fft(int* x, double* X_real, double* X_imag, unsigned int N, unsigned int s) {
     unsigned int k;
-    double t_real;
-    double t_imag;
    
     // At the lowest level pass through (delta T=0 means no phase).
     if (N == 1) {
         X_real[0] = x[0];
-        X_imag[0] = x[0];
+      //  cout << endl << X_real[0] << "    " << X_imag[0]  << endl;
         return;
     }
 
@@ -50,27 +79,38 @@ void fft(int* x, double* X_real, double* X_imag, unsigned int N, unsigned int s)
     for (k = 0; k < N / 2; k++) {
         t_real = X_real[k];
         t_imag = X_imag[k];
-        X_real[k] = t_real + cos(-2 * M_PI * k / N) * X_real[k + N / 2];
-        X_imag[k] = t_imag + sin(-2 * M_PI * k / N) * X_imag[k + N / 2];
-        X_real[k + N / 2] = t_real - cos(-2 * M_PI * k / N) * X_real[k + N / 2];
-        X_imag[k + N / 2] = t_imag - sin(-2 * M_PI * k / N) * X_imag[k + N / 2];
-
+        X_real[k] = t_real + cos(-2 * M_PI * k / N) * X_real[k + N/2];
+        X_imag[k] = t_imag + sin(-2 * M_PI * k / N) * X_imag[k + N/2];
+        X_real[k + N / 2] = t_real + cos(-2 * M_PI * k / N) * X_real[k + N / 2];
+        X_imag[k + N / 2] = t_imag + sin(-2 * M_PI * k / N) * X_imag[k + N / 2];
+      //  cout << endl << X_real[k] << "    " << X_imag[k] << "   " << k << endl;
+      //  cout << endl << X_real[k + N / 2] << "    " << X_imag[k + N / 2] << "   " << k << endl;
     }
 
 }
-
-void reverseBits(int* n)
-{
-    int size = sizeof(n) / sizeof(n[0]);
-    size = 8;
-    int bit_number = 3;
-    int temp = 0;
-    for (int i = 0; i < size; i++)
-    {
-        int x = i;
-         x= (x<< bit_number) | i;
-         cout << endl <<i << "    "  << x << endl;
+void fft_process(int* x,complex<double>* X, unsigned int N, unsigned int s) {
+    unsigned int k;
+    complex<double> t;
+    complex<double> J(0, 1);
+    // At the lowest level pass through (delta T=0 means no phase).
+    if (N == 1) {
+        X[0] = x[0];
+        return;
     }
+
+    // Cooley-Tukey: recursively split in two, then combine beneath.
+    fft_process(x, X, N / 2, 2 * s);
+    fft_process(x + s, X + N / 2, N / 2, 2 * s);
+
+    for (k = 0; k < N / 2; k++) {
+        t = X[k];
+        X[k] = t + exp(-2 * M_PI  *J* k / N) * X[k + N / 2];
+        X[k + N / 2] = t - exp(-2 * M_PI * J * k / N) * X[k + N / 2];
+    }
+}
+void fft(int* x, complex<double>* X, unsigned int N)
+{
+    fft_process( x, X, N, 1);
 }
 
 int main() {
@@ -85,13 +125,11 @@ int main() {
 
     }*/
     fourir<8>(input,real,imag);
-    
+    complex<double>i(1, 1);
+    complex<double>i2(2, 2);
     int in[8] = { 1,2,3,4,5,6,7,8 };
-    double re[8];
-    reverseBits(in);
-    double im[8];
-    fft(in, re, im,8,1 );
-    cout << endl << re[5] << "    " << im[5] << "   " << endl;
-    reverseBits(in);
+    complex<double> out[8];
+    fft(in, out, 8);
+    cout << out[0] << endl << out[3]<<endl;
     return 0;
 }
