c9ee088376d8b34261f6a4be18a68e117fd531a9
moamen-gamal <moameneisa@yahoo.com>
2021-05-05 18:12:39 +0200

    "up"

--- pytest.py
+++ pytest.py
@@ -1,13 +1,24 @@
 import ctypes 
-import numpy as np
-import time
+from ctypes import c_void_p, c_double, c_int, cdll
 from numpy.ctypeslib import ndpointer
+import numpy as np
+from numpy import random
+import time
 library =ctypes.CDLL('./outputFile.so')
-library.connect()
-a = np.array([1,2,3,4,5,6,7,8,9,10])
-ft = library.fourir
-ft.restype=None
-time1=time.time()
-ft(ctypes.c_void_p(a.ctypes.data))
-time2=time.time()-time1
-print(time2)
+a = np.array([1,2,3,4,5,6,7,8])
+fou= library.fourir
+fou.restype = None
+arr_sizes =np.array([2**0])
+for i in range(1,15):
+    arr_sizes=np.append(arr_sizes,2**i)
+fourir_slow_time =np.array(0)
+fft_time =np.array(0)
+for i in range(0,16):
+    a=random.randint(5, size=(arr_sizes[i]))
+    time1 =time.time()
+    size =arr_sizes[i]
+    fou(c_void_p(a.ctypes.data),ctypes.c_int(size))
+    time2=time.time()
+    time3 = tim2 - time1
+    fourir_slow_time=np.append(fourir_slow_time,time3)
+print(fourir_slow_time)
--- test.cpp
+++ test.cpp
@@ -1,35 +1,56 @@
 #define _USE_MATH_DEFINES
-#include <cmath>
 #include<iostream>
 #include<array>
+#include<complex.h>
+#include<cmath>
 using namespace std;
+
+#define I _Complex_I // can be used to redefine I
+using namespace std;
+
+// Trick to allow type promotion below
+template <typename T>
+struct identity_t { typedef T type; };
+
+/// Make working with std::complex<> nubmers suck less... allow promotion.
+#define COMPLEX_OPS(OP)                                                 \
+  template <typename _Tp>                                               \
+  std::complex<_Tp>                                                     \
+  operator OP(std::complex<_Tp> lhs, const typename identity_t<_Tp>::type & rhs) \
+  {                                                                     \
+    return lhs OP rhs;                                                  \
+  }                                                                     \
+  template <typename _Tp>                                               \
+  std::complex<_Tp>                                                     \
+  operator OP(const typename identity_t<_Tp>::type & lhs, const std::complex<_Tp> & rhs) \
+  {                                                                     \
+    return lhs OP rhs;                                                  \
+  }
+COMPLEX_OPS(+)
+COMPLEX_OPS(-)
+COMPLEX_OPS(*)
+COMPLEX_OPS(/ )
+#undef COMPLEX_OPS
+
 extern "C"
 {
     void connect();
-    //int* add (int a[5],int b[5]);
-    void fourir(double input_data[],double output[]);
-    
+    void fourir( double* input_data,int size);
+    void fft( int* x, complex<double>* X, unsigned int N);
+    void fft_process( int* x,complex<double>* X, unsigned int N, unsigned int s);
 }
 void connect()
 {
     printf("connect\n");
 }
-/*int* add(int* a,int* ,int sum)
+
+void fourir(double* input_data,int size)
 {
-    int*c =new int[5];
-    for(int i =0;i<5;i++)
-    {
-        cout<<a[i]<<"   "<<b[i]<<endl;
-        c[i]=a[i]+b[i];
-    }
-    c=sum
-    return c;
-}*/
-void fourir(int input_data[],int output[])
-{
-    int size =sizeof(input_data)/sizeof(input_data[0]);
-    double real[];
-    double imag[];
+    if (input_data[0]==1)
+        cout<<"right"<<endl;
+    const int s=size;
+    double real[s];
+    double imag[s];
     for (int k = 0; k < 10; k++)
     {
         double re= 0;
@@ -46,8 +67,28 @@ void fourir(int input_data[],int output[])
         cout << endl << re << "    " << im << "   " <<k<<endl;
         real[k] = re;
         imag[k] = im/10;
-        output[k]=real[k];
     }
     
-    return;
+}
+void fft( int* x, complex<double>* X, unsigned int N)
+{
+    fft_process( x, X, N, 1);
+}
+void fft_process( int* x,complex<double>* X, unsigned int N, unsigned int s) {
+    unsigned int k;
+    complex<double> t;
+    complex<double> J(0, 1);
+    if (N == 1) {
+        X[0] = x[0];
+        return;
+    }
+
+    fft_process(x, X, N / 2, 2 * s);
+    fft_process(x + s, X + N / 2, N / 2, 2 * s);
+
+    for (k = 0; k < N / 2; k++) {
+        t = X[k];
+        X[k] = t + exp(-2 * M_PI  *J* k / N) * X[k + N / 2];
+        X[k + N / 2] = t - exp(-2 * M_PI * J * k / N) * X[k + N / 2];
+    }
 }
